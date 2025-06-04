37326d430f81f72a43700bcd1a77154e7af7e11a
limseeeun <limseeun566@gmail.com>
2025-06-04 15:46:39 +0900

    커뮤니티끝

--- community/serializers.py
+++ community/serializers.py
@@ -7,6 +7,8 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+        read_only_fields = ['author']  # ✅ 추가
+
     
     def get_likes_count(self, obj):
         return obj.like_set.count()
@@ -15,8 +17,11 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = '__all__'
+        read_only_fields = ['user']   # ✅ user를 클라이언트 요청에서 제외
+
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+        read_only_fields = ['author']   # ✅ author를 read_only로 지정
